abaa3883e0b1d93bba62f12d0798b0b1f219458f
unknown <diksha.novasoftcorps@gmail.com>
2018-01-15 12:16:04 +0530

    changes

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-  url: 'mongodb://meenal:meenal@ec2-54-234-40-96.compute-1.amazonaws.com:27017/dummyDB',
+url: 'mongodb://meenal:meenal@ec2-54-234-40-96.compute-1.amazonaws.com:27017/dummyDB',
 
 };
